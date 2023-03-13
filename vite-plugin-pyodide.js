20de859ea8723a080548ede33eee42276565e993
pauleveritt <paul.everitt@jetbrains.com>
2023-03-12 21:22:17 -0400

    Minor cleanups. Remove some unused machinery.

---
+++ vite-plugin-pyodide.js
@@ -0,0 +1,30 @@
+import fs from "fs";
+import path from "path";
+import { createRequire } from "module";
+
+const PYODIDE_ASSETS = [
+  "pyodide.asm.js",
+  "pyodide.asm.wasm",
+  "pyodide.asm.data",
+  "pyodide_py.tar",
+  "pyodide.mjs",
+  "repodata.json",
+];
+const getModuleDir = () => {
+  const require = createRequire(import.meta.url);
+  const lookupPaths = require.resolve
+    .paths("pyodide")
+    .map((p) => path.join(p, "pyodide"));
+  return lookupPaths.find((p) => fs.existsSync(p));
+};
+
+export function getPyodideAssets() {
+  const pyodideDir = getModuleDir();
+  return PYODIDE_ASSETS.map((asset) => {
+    return {
+      src: path.join(pyodideDir, asset),
+      dest: "pyodide-data",
+      rename: asset,
+    };
+  });
+}
