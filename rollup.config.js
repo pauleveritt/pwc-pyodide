df7a4bba59396fdefae47c29e45d4a81002f9a7e
pauleveritt <paul.everitt@jetbrains.com>
2023-03-12 20:59:04 -0400

    Some cleanup.

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,12 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-
-
-// `npm run build` -> `production` is true
-// `npm run dev` -> `production` is false
-const production = !process.env.ROLLUP_WATCH;
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
 
 export default {
-	input: 'src/pwc_pyodide/main.js',
-	output: {
-		dir: 'public',
-		format: 'es', // immediately-invoked function expression — suitable for <script> tags
-		sourcemap: true
-	},
-	plugins: [
-		json(),
-		resolve(), // tells Rollup how to find date-fns in node_modules
-		commonjs(), // converts date-fns to ES modules
-	],
-	external: ['fs/promises']
+  plugins: [
+    json(),
+    resolve(), // tells Rollup how to find date-fns in node_modules
+    commonjs(), // converts date-fns to ES modules
+  ],
+  external: ["fs/promises"],
 };
--- src/pwc_pyodide/update.js
+++ src/pwc_pyodide/update.js
@@ -9,8 +9,8 @@ async function setupPyodide() {
   pyodide.runPython(`
 		import js
 		div = js.document.createElement("div")
-		div.innerHTML = "<h1>Hello Pyodide!!! This element was created from Python</h1>"
-		js.document.body.prepend(div)
+		div.innerHTML = "<p><em>Hello Pyodide! This element was created from Python</</p>"
+		js.document.body.append(div)
 	`);
 }
 
