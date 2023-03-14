4c5a5219594e3e88fd324030dd4ab7a2bd656c32
pauleveritt <paul.everitt@jetbrains.com>
2023-03-13 20:21:42 -0400

    Remove unused dependencies.

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,6 @@ export default defineConfig({
   build: {
     outDir: "../../dist",
     emptyOutDir: true,
-    rollupOptions: {
-      // plugins: [resolve(), commonjs()],
-    },
   },
   plugins: [
     viteStaticCopy({
